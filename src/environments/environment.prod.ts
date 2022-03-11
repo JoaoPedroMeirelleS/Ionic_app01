00a59e154ffec960807d8891265f0eb7b0628fa9
JoaoPedroMeirelleS <94013155+JoaoPedroMeirelleS@users.noreply.github.com>
2022-03-11 16:51:09 -0300

    20220311_001

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'app01-bd58f',
+    appId: '1:1020110281640:web:1c423676069ee9c0f35b9d',
+    storageBucket: 'app01-bd58f.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDbTTCa4B-Sx-vHIsEQn6FA2j_k01Yjb6o',
+    authDomain: 'app01-bd58f.firebaseapp.com',
+    messagingSenderId: '1020110281640',
+  },
   production: true
 };
